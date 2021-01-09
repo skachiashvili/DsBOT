47457c5f332f51e2093298542b00ef42b725e362
skachiashvili <s.kachiashvili@gtu.edu.ge>
2021-01-09 22:47:30 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const { Client } = require ('discord.js');
+const client = new Client();
+client.once ('ready', () => {
+    console.log(`${client.user.tag} has logged in.`);
+});
+client.on('message', (message)=>{
+
+    if (message.author.tag==='darkman899#6697') {
+        message.react('🚮');
+    }
+   /* else {
+        message.react('👍');
+    }*/
+} );
+
+
+
+
+
+
+
+
+client.login('Nzk3NDUyNDU2MTE1OTYxODc3.X_mreQ.2f6yt1xq6ZCdiZ40ABoia_ZBN2w');
